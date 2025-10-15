12d3597650ef7108a851ccae7ce2e9cbc3b29248
mahmoud-saber <mahmoudsabersd.12345@gmil.com>
2025-10-16 01:15:45 +0300

    update

--- resources/views/post/show-post.blade.php
+++ resources/views/post/show-post.blade.php
@@ -7,13 +7,16 @@
                 <td>{{ $post->title }}</td>
                 <td>{{ $post->content }}</td>
                 <td>{{ $post->created_at }}</td>
-                <td><a  class="btn btn-outline-danger"href="{{ route('post.index') }}">Back</a></td>
-                <td><a class="btn btn-danger" href="{{ route('post.destroy', $post->id) }}">Delete</a></td>
-              <td><a  class="btn btn-outline-success"href="{{ route('post.edit', $post->id) }}">Edit</a></td>
+                <td><a class="btn btn-outline-danger"href="{{ route('post.index') }}">Back</a></td>
+                <td>
+                    <form action="{{ route('post.destroy', $post) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                </td>
+                <td><a class="btn btn-outline-success"href="{{ route('post.edit', $post->id) }}">Edit</a></td>
 
             </tr>
         </tbody>
     </table>
-    
-    
 @endsection
